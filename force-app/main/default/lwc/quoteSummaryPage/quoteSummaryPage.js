c673089bc46c89f3bc1fa4a0cf946774a3f082ec
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-02-18 21:45:08 -0600

    Aura Component Parent and LWC Child with Quote Lines

--- force-app/main/default/lwc/quoteSummaryPage/quoteSummaryPage.js
+++ force-app/main/default/lwc/quoteSummaryPage/quoteSummaryPage.js
@@ -1,7 +1,36 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement,api,wire,track } from 'lwc';
+import {CurrentPageReference} from 'lightning/navigation'
+import getQuoteLines from '@salesforce/apex/QuoteLinesController.getQuoteLines';
 
 export default class QuoteSummaryPage extends LightningElement {
-    @api recordId;
-    @api quoteId; 
-    @api title; 
+    @track quoteLines;
+    @track columns =[
+        {label:'Id', fieldName: 'Id', type: 'text'}, 
+        {label:'Product Name', fieldName:'SBQQ__ProductName__c',type: 'text' },
+        {label:'Description', fieldName: 'SBQQ__Description__c',type: 'text'},
+        {label:'Qty', fieldName: 'SBQQ__Quantity__c',type: 'text'}
+    ];
+
+    @wire(getQuoteLines,{quoteId:'$quoteIden'})
+    quoteLinesHandler({data,error}){
+        if(data){
+            console.log(data)
+            this.quoteLines = data; 
+            
+        }
+        if(error){
+            console.error(error)
+        }
+    }
+
+    @wire(CurrentPageReference)
+    pageRef
+
+    get PageReference(){
+        return this.pageRef ? JSON.stringify(this.pageRef,null,2):''
+    }
+
+    get quoteIden(){
+        return (this.pageRef.state.c__quoteId)
+    }
 }
